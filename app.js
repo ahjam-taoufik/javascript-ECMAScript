42feb9235b3751ac487d10184d57db86b761068f
taoufik ahjam <taoufikahjam@gmail.com>
2021-12-31 22:19:22 +0100

    import export outside nodejs

---
+++ app.js
@@ -0,0 +1,30 @@
+
+//methode 1
+//  export const num=12;
+
+//  export const nums=[1,45,8,4]
+
+//  export function func(name){
+//      return `salam alikoum ${name}`
+//  }
+
+
+ //methode2 =================================
+
+  const num=12;
+
+ const nums=[1,45,8,4]
+
+ function func(name){
+     return `salam alikoum ${name}`
+ }
+
+ export{
+     num,nums,func
+ }
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,10 @@
+//you should be add type="module" in script tag in index.html 
+
+import{func,nums,num} from './app.js'
+
+
+console.log(func('ahmed'))
+console.log(nums)
+console.log(num)
+
+
