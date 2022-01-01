eddbeabccb76890bca9acedb657438ec2d31c154
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-01 01:20:49 +0100

    Call Stack, Web API, Event Loop, Callback Queue

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-import User from "./User.js";
 
+// Example One
 
-const user1=new User('ahmed',14,'discord')
+let i = 10;
+let j = 20;
+console.log(i + j);
 
-console.log(user1.app);
+function first() {
+  console.log("Hello First");
+}
+
+function second() {
+  first();
+  console.log("Hello Second");
+}
+
+second();
+
+// Example Two
+
+window.setTimeout(() => console.log("Last"), 1000);
+console.log("One");
+window.setTimeout(() => console.log("Two"), 0);
+console.log("Three");
+
+// Example Three
+
+window.setTimeout(() => console.log(myVariable), 0);
+
+let myVariable = 100;
+
+myVariable += 200;
